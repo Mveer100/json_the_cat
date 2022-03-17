0494faaae5ac1891c54672e3145f6b66a37b6ae8
Maxwell Veer <maxaveer@gmail.com>
2022-03-17 19:49:31 +0000

    implemented the breedFetcher function to take in a command line argument that is a cat breed name and give info about it

---
+++ breedFetcher.js
@@ -0,0 +1,17 @@
+const request = require("request");
+const breedName = process.argv[2];
+const dynamicBreedSearch = (breedName) => {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, (error, repsonse, body) => {
+    if (error) {
+      console.log("error:", error);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      console.log(`Error: Search could not find that breed`, error);
+      return;
+    }
+    console.log(data[0]["description"]);
+  });
+};
+dynamicBreedSearch(breedName);
